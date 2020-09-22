edf58b0a8ddf547c29e9205f2ea05de114144db1
Franco Casas <casassfranco@gmail.com>
2020-09-22 02:53:55 -0300

    Se convirtio en unique el nombreNivel

--- src/models/nivelUsuario.js
+++ src/models/nivelUsuario.js
@@ -2,7 +2,7 @@ import mongoose, { Schema } from "mongoose";
 
 const nivelUsuarioSchema = new Schema(
   {
-    nombreNivel: { type: String, maxlength: 15, required: true},
+    nombreNivel: { type: String, maxlength: 15, required: true, unique: true },
     estadoNivel: { type: Boolean, default: true, required: true }
   },
   { timestamps: true }
